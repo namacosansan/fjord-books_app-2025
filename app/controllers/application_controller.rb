0a90021f13c423b633fe9cf793d255e2ffabd758
namacosansan <namacosansan@gmail.com>
2025-12-24 17:11:40 +0900

    books画面をi18n対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+  
   private
 
   def switch_locale(&action)
