ca70c9ec3a912bcd712ec71e4f04ce897964f9e9
namacosansan <namacosansan@gmail.com>
2025-12-25 14:41:32 +0900

    RuboCopとerb_lintを使って指摘事項を修正し、UIを整理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   around_action :switch_locale
 
   def default_url_options
     { locale: I18n.locale }
   end
-  
+
   private
 
   def switch_locale(&action)
