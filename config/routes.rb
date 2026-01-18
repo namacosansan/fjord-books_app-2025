095fc5e7036732fe65c537c2f0b8960ce12e1930
namacosansan <namacosansan@gmail.com>
2026-01-18 12:10:49 +0900

    ユーザーページを作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
+  get "home/index"
+  devise_for :users
   resources :books
+  resources :users, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +17,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "books#index"
 end
