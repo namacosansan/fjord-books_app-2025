3f3a8c8566b78159f0714ac020767cf95fb49d82
namacosansan <namacosansan@gmail.com>
2026-01-24 09:51:48 +0900

    Revert "Merge pull request #1 from namacosansan/my-i18n"
    This reverts commit 346419d5f2c987797fa522454b3accf7e131d4ce, reversing
    changes made to 5232b9e1d205ffa48ae6e17eadab62bb459e9f08.

--- config/initializers/locale.rb
+++
@@ -1,7 +0,0 @@
-I18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
-
-# 利用するロケール
-I18n.available_locales = [:ja, :en]
-
-# デフォルトロケール（URLにlocale指定がないときに使われる）
-I18n.default_locale = :ja
