54413a0dbfcf5ffe229cf32d7956b922c3418e7c
namacosansan <namacosansan@gmail.com>
2026-01-31 13:09:48 +0900

    rubocopによる修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "books#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
