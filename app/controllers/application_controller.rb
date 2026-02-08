ddcfff61b6b2540436ff6af8c9c540bc3a8654f5
namacosansan <namacosansan@gmail.com>
2026-02-08 09:28:29 +0900

    ユーザーが自分のアイコン画像をアップロードできるようにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     keys = %i[name postal_code address self_introduction]
-    devise_parameter_sanitizer.permit(:sign_up, keys:)
-    devise_parameter_sanitizer.permit(:account_update, keys:)
-  end
+    devise_parameter_sanitizer.permit(:sign_up, keys:%i(avatar))
+    devise_parameter_sanitizer.permit(:account_update, keys:%i(avatar))
+   end
 
   private
 
