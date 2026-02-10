d428d92a213aa937242f136920c44e98210e2bb3
namacosansan <namacosansan@gmail.com>
2026-02-10 18:10:57 +0900

    rubocopとerb_lintによる軽微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  
-  validates :avatar, content_type: ['image/png', 'image/jpeg', 'image/gif']
 
+  validates :avatar, content_type: ['image/png', 'image/jpeg', 'image/gif']
 end
