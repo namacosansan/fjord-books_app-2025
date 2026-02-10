14736897285c921bb671c09f44c4567df8f20ca2
namacosansan <namacosansan@gmail.com>
2026-02-10 17:36:07 +0900

    アップロード可能なファイルをjpg, png, gifの3種類だけに限定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  
+  validates :avatar, content_type: ['image/png', 'image/jpeg', 'image/gif']
+
 end
