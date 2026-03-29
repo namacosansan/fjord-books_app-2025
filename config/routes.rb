e855f1693443337398a950c428dc0a45e15e2190
namacosansan <namacosansan@gmail.com>
2026-03-29 09:03:04 +0900

    HTML5の必須チェック機能を追加、config/routes.rbの重複項目の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
   resources :users, only: %i(index show)
-  resources :reports
   resources :reports do
     # /reports/:report_id/comments
     resources :comments, only: [:create, :destroy]
