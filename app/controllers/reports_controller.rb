6400b452f1dbff8904575b06dbc8d9d5b5475155
namacosansan <namacosansan@gmail.com>
2026-03-30 15:37:18 +0900

    rubocopによる軽微修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@
 
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
-  before_action :authorize_report_owner!, only: %i[edit update destroy]  
+  before_action :authorize_report_owner!, only: %i[edit update destroy]
 
   # GET /reports
   def index
