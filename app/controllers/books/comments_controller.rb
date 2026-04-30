504c9b5cd05639099094e5865441c889ad04911c
namacosansan <namacosansan@gmail.com>
2026-04-30 14:35:50 +0900

    params.requireからparams.expectに変更

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -33,6 +33,6 @@ class Books::CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.expect(comment: [:body])
   end
 end
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -33,6 +33,6 @@ class Reports::CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.expect(comment: [:body])
   end
 end
